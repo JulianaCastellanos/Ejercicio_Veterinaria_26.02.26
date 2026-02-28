89be3c1a75f4130b69baf23ec41d436c4d904754
JuliCaste <julianacastellanos0108@gmail.com>
2026-02-27 20:12:16 -0500

    Puliendo detalles

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,6 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
 
-
         Scanner teclado = new Scanner(System.in);
 
         //Actividad veterinaria - 26/02/26
@@ -16,22 +15,20 @@ public class Main {
 
         do {
 
+            System.out.println("");
             System.out.println("------- Menu --------");
             System.out.println("");
 
             System.out.println("1. Crear mascota");
             System.out.println("2. Mostrar lista");
-
-            System.out.println("3. Modificar edad");
-            System.out.println("4. Modificar peso");
-            System.out.println("5. Cambiar estado de salud");
-            System.out.println("6. Mostrar información");
-            System.out.println("7. Salir");
+            System.out.println("3. Buscar objeto");
+            System.out.println("4. Salir");
             System.out.println("");
             System.out.println("Seleccione una opción");
             op = teclado.nextInt();
 
             switch (op) {
+
                 case 1:
 
                     System.out.println("Ingrese el nombre de su mascota:");
@@ -63,90 +60,110 @@ public class Main {
 
                 case 3:
 
+                    String nombre1;
+                    System.out.println("Ingrese el nombre de la mascota");
+                    nombre1 = teclado.next();
+                    if (lstLista.isEmpty()){
+                        System.out.println("La lista no tiene elementos");
+                    }
+
+                    Mascota mas1 = null;
+
+                    for (Mascota m: lstLista) {
+                        if (m.getNombre().equalsIgnoreCase(nombre1)) {
+                            mas1 = m;
+                            System.out.println(mas1.toString());
+                        }
+                    }
+
+                    if (mas1 == null) {
+                        System.out.println("Mascota no encontrada");
+                        break;
+                    }
+
+                    int opc;
+
+                    do {
+
+                        System.out.println("------- Menu --------");
+                        System.out.println("");
+
+                        System.out.println("1. Modificar edad");
+                        System.out.println("2. Modificar peso");
+                        System.out.println("3. Cambiar estado de salud");
+                        System.out.println("4. Mostrar información");
+                        System.out.println("5. Salir");
+
+                        System.out.println("");
+
+                        System.out.println("Ingrese el número de la opción que desea realizar");
+                        opc = teclado.nextInt();
+
+                        if (opc==1) {
+
+                            System.out.println("1. Cambiar edad");
+                            System.out.println("2. Cumplió años");
+                            int opci = teclado.nextInt();
+                            if (opci==1) {
+                                System.out.println("Ingrese el nuevo valor de la edad");
+                                mas1.setEdad(teclado.nextInt());
+                            } else if (opci == 2) {
+                                System.out.println("Felíz cumple");
+                                mas1.cumplirAnios();
+                            } else {
+                                System.out.println("Opción invalida");
+                            }
+
+                        } else if (opc ==2) {
+
+                            System.out.println("1. Aumentar peso");
+                            System.out.println("2. Disminuir peso");
+                            int opci = teclado.nextInt();
+
+                            if (opci == 1){
+                                System.out.println("¿Cuánto aumentó?");
+                                double peso1 = teclado.nextDouble();
+                                mas1.engordar(peso1);
+                            } else if (opci == 2) {
+                                System.out.println("¿Cuánto peso perdió?");
+                                double peso2 = teclado.nextDouble();
+                                mas1.adelgazar(peso2);
+                            } else {
+                                System.out.println("Error");
+                            }
+
+                        } else if (opc==3) {
+
+                            System.out.println("Ingrese el estado de la mascota");
+                            System.out.println("1. Sano");
+                            System.out.println("2. Enfermo");
+                            System.out.println("3. recuperado");
+                            int estado = teclado.nextInt();
+
+                            if (estado == 1) {
+                                mas1.setSaludable(true);
+                            } else if (estado == 2 ) {
+                                mas1.enfermar(true);
+                            } else if (estado == 3) {
+                                mas1.recuperarSalud(true);
+                            } else {
+                                System.out.println("error");
+                            }
+
+                        } else if (opc==4) {
+                            mas1.mostrarFicha();
+                        } else if (opc ==5) {
+                            System.out.println("Regresando al menú principal...");
+                        }else {
+                            System.out.println("Error, opción invalida");
+                        }
+
+                    } while (opc !=5);
                 case 4:
-
-                case 5:
-
-                case 6:
-
-                case 7:
-
+                    System.out.println("Bye bye :)");
             }
 
-        } while (op != 7);
-
-
-
-
-
-        /*    do {
-
-            System.out.println("------- Menu --------");
-            System.out.println("");
-
-            System.out.println("1. Modificar edad");
-            System.out.println("2. Modificar peso");
-            System.out.println("3. Cambiar estado de salud");
-            System.out.println("4. Mostrar información");
-            System.out.println("5. Salir");
-
-            System.out.println("");
-
-            System.out.println("Ingrese el número de la opción que desea realizar");
-            op = teclado.nextInt();
-
-            if (op==1) {
-                System.out.println("1. Cambiar edad");
-                System.out.println("2. Cumplió años");
-                int opc = teclado.nextInt();
-                if (opc==1) {
-                    System.out.println("Ingrese el nuevo valor de la edad");
-                    m1.setEdad(teclado.nextInt());
-                } else if (opc == 2) {
-                    System.out.println("Felíz cumple");
-                    m1.cumplirAnios();
-                } else {
-                    System.out.println("Opción invalida");
-                }
-            } else if (op ==2) {
-                System.out.println("1. Aumentar peso");
-                System.out.println("2. Disminuir peso");
-                int opci = teclado.nextInt();
-                if (opci == 1){
-                    System.out.println("¿Cuánto aumentó?");
-                    double peso = teclado.nextDouble();
-                    m1.engordar(peso);
-                } else if (opci == 2) {
-                    System.out.println("¿Cuánto peso perdió?");
-                    double peso2 = teclado.nextDouble();
-                    m1.adelgazar(peso2);
-                } else {
-                    System.out.println("Error");
-                }
-            } else if (op==3) {
-                System.out.println("Ingrese el estado de la mascota");
-                System.out.println("1. Sano");
-                System.out.println("2. Enfermo");
-                System.out.println("3. recuperado");
-                int estado = teclado.nextInt();
-                if (estado == 1) {
-                    m1.setSaludable(true);
-                } else if (estado == 2 ) {
-                    m1.enfermar(true);
-                } else if (estado == 3) {
-                    m1.recuperarSalud(true);
-                } else {
-                    System.out.println("error");
-                }
-            } else if (op==4) {
-                m1.mostrarFicha();
-            } else if (op ==5) {
-                System.out.println("Bye bye :)");
-            }else {
-                System.out.println("Error, opción invalida");
-            }
-        } while (op !=5);
-    */
+        } while (op != 4);
 
     }
 }
